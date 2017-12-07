9166ec78d9c058269a00d63bbea6689f0daf742d
fabio <fabioteixeirahl@gmail.com>
2017-12-07 02:21:27 +0000

    updade random tests

--- src_random/ex_test2.asm
+++ src_random/ex_test2.asm
@@ -5,16 +5,35 @@
 	
 	.section directdata
 	.org 2
-	
+aux: .byte 1
+
 	.text
 main:
-	ldih r0,#high(time) ;coloca na parte alta de r0 12
-	ldi r0,#low(time);coloca na parte baixa de r0 10 e esmaga a parte alta com zeros
+	;ldih r0,#high(time) ;coloca na parte alta de r0 12
+	;ldi r0,#low(time);coloca na parte baixa de r0 10 e esmaga a parte alta com zeros
 	
-	ldi r1,#low(time);coloca na parte baixa de r0 10
-	ldih r1,#high(time);coloca na parte alta de r0 12
+	;ldi r1,#low(time);coloca na parte baixa de r0 10
+	;ldih r1,#high(time);coloca na parte alta de r0 12
 	
+	ldi r1, #0
+	ldb r0, aux
+	shr r6, r0, #1, 0
+	jnc r7, #2	
+	;addf r1, r1, #1
+	incf r1
+	jc r7, #1
+	;sub r1, r1, #1
+	decf r1
 	
-	
+	ldi r1, #0
+	st r1, aux
+	ldb r0, aux
+	shr r6, r0, #1, 0
+	jnc r7, #2	
+	;addf r1, r1, #1
+	incf r1
+	jc r7, #1
+	;sub r1, r1, #1
+	decf r1
 	jmp $
    	.end
